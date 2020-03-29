64e28bb9df9c4e9bdcd3a5bafaed425282566595
msh.vlsv <maria.vlasova.02@gmail.com>
2020-03-29 17:06:25 +0200

    initial commit

---
+++ app/src/main/java/com/bignerdranch/samodelkin/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.bignerdranch.samodelkin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bignerdranch.samodelkin.CharacterGenerator.fetchCharacterData
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.android.UI
+import kotlinx.coroutines.launch
+
+private const val CHARACTER_DATA_KEY = "CHARACTER_DATA_KEY"
+
+private var Bundle.characterData
+    get() = getSerializable(CHARACTER_DATA_KEY) as CharacterGenerator.CharacterData
+    set(value) = putSerializable(CHARACTER_DATA_KEY, value)
+
+class MainActivity : AppCompatActivity() {
+
+    private var characterData = CharacterGenerator.generate()
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.characterData = characterData
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        characterData = savedInstanceState?.characterData ?: CharacterGenerator.generate()
+        displayCharacterData()
+        generateButton.setOnClickListener {
+            launch(UI) {
+                characterData = fetchCharacterData().await()
+                displayCharacterData()
+            }
+        }
+    }
+
+    private fun displayCharacterData() {
+        characterData.run {
+            nameTextView.text = name
+            raceTextView.text = race
+            dexterityTextView.text = dex
+            wisdomTextView.text = wis
+            strengthTextView.text = str
+        }
+    }
+}
